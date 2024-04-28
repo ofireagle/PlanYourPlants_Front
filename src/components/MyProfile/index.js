5d3c5f5506ef5f30f86101405f252278d74c2903
Yarin Kahiri <Kahiriyarin@gmail.com>
2024-04-28 16:07:04 +0300

    Updates

--- src/components/MyProfile/index.js
+++ src/components/MyProfile/index.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-<<<<<<< HEAD
 import { useNavigate } from 'react-router-dom';
 import { Container, FormWrap, FormContent, Form, FormH1, FormLabel, FormInput, FormButton, HeroBg, VideoBg, FormSelect } from './MyProfileElements';
 import Video from '../../videos/video6.mp4';
@@ -18,34 +17,6 @@ const MyProfile = () => {
     const [cities, setCities] = useState([]);
     const [currentUser, setCurrentUser] = useState({});
     const [selectedCity, setSelectedCity] = useState('');
-=======
-import {Navigate, useNavigate} from 'react-router-dom'
-import {
-    Container,
-    FormWrap,
-    FormContent,
-    Form,
-    FormH1,
-    FormLabel,
-    FormInput,
-    FormButton,
-    HeroBg,
-    VideoBg
- } from './MyProfileElements'
- import Video from '../../videos/video6.mp4'
- import { createAxiosInstance, isAuthenticated} from '../../services/api';
-
-const MyProfile = () => {
-  const navigate = useNavigate();
-  const [state, setState] = useState(null);
-  const [statePlants, setStatePlants] = useState([]);
-  const [inputName, setInputName] = useState('');
-  const [inputEmail, setInputEmail] = useState('');
-  const [inputPhone, setInputPhone] = useState('');
-  const [inputCity, setInputCity] = useState('');
-  const [inputCountry, setInputCountry] = useState('');
-  const [currentUser, setCurrentUser] = useState({});
->>>>>>> 2c09b9933050870816aba3b1dd88fe68b79fb05e
 
     useEffect(() => {
         const fetchData = async () => {
@@ -78,7 +49,6 @@ const MyProfile = () => {
                     setStatePlants(plantsResp.data.details);
                 }
 
-<<<<<<< HEAD
                 // Fetch countries
                 const countriesResp = await axios.get("https://countriesnow.space/api/v0.1/countries");
                 setCountries(countriesResp.data.data);
@@ -86,57 +56,13 @@ const MyProfile = () => {
                 console.log(error);
             }
         };
-=======
-  const getUserByToken = async () => {
-    const path = '/users/getUserByToken';
-    try {
-      const axiosInstance = createAxiosInstance();
-      const resp = await axiosInstance.get(path);
-      if (resp.data.status === 'success') {
-        setCurrentUser(resp.data.details);
-        setInputName(resp.data.details.name);
-        setInputEmail(resp.data.details.email);
-        setInputPhone(resp.data.details.phone);
-        setInputCity(resp.data.details.city);
-        setInputCountry(resp.data.details.country);
-        setState(resp.data.details)
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  };
->>>>>>> 2c09b9933050870816aba3b1dd88fe68b79fb05e
 
         fetchData();
     }, [navigate]);
 
-<<<<<<< HEAD
     const fetchCities = async (country) => {
         const selectedCountryObj = countries.find(c => c.country === country);
         setCities(selectedCountryObj ? selectedCountryObj.cities : []);
-=======
-  const updateApi = async (data) => {
-    const path = '/users/updateUser';
-    try {
-      const axiosInstance = createAxiosInstance();
-      const resp = await axiosInstance.patch(path, data);
-      console.log(resp.data);
-      alert('User details updated successfully!');
-    } catch (error) {
-      console.log(error);
-      alert('Error updating user details.');
-    }
-  };
-
-  const handleFetchData = async () => {
-    const userData = {
-      user_id: currentUser._id,
-      name: inputName,
-      email: inputEmail,
-      phone: inputPhone,
-      city: inputCity,
-      country: inputCountry,
->>>>>>> 2c09b9933050870816aba3b1dd88fe68b79fb05e
     };
 
     const handleCountryChange = (e) => {
@@ -147,7 +73,6 @@ const MyProfile = () => {
         }
     };
 
-<<<<<<< HEAD
     const handleFetchData = async () => {
         const userData = {
             user_id: currentUser._id,
@@ -157,89 +82,6 @@ const MyProfile = () => {
             city: selectedCity,
             country: selectedCountry
         };
-=======
-  return (
-    <Container>
-        <HeroBg>
-                <VideoBg autoPlay loop muted src={Video} type='video/mp4'/>
-        </HeroBg>
-        <FormWrap>
-            <FormContent>
-                <Form action="#">
-                    <FormH1> My Gold </FormH1>
-                     <FormLabel type='gold'> {state && state.newPlants.length * 10 + 50} </FormLabel>
-                </Form>
-            </FormContent>
-        </FormWrap>
-        <FormWrap onSubmit={(e) => {
-          e.preventDefault();
-          // handleFetchData();
-        }}>
-            <FormContent>
-                <Form action="#">
-                <FormH1> My Profile </FormH1>
-            <FormLabel htmlFor='name'> Name </FormLabel>
-            <FormInput
-              id='name'
-              defaultValue={inputName}
-              autoComplete='on'
-              onChange={(e) => setInputName(e.target.value)}
-              required
-            />
-            <FormLabel htmlFor='email'> Email </FormLabel>
-            <FormInput
-              id='email'
-              defaultValue={inputEmail}
-              autoComplete='on'
-              onChange={(e) => setInputEmail(e.target.value)}
-              required
-            />
-            <FormLabel htmlFor='phone'> Phone </FormLabel>
-            <FormInput
-              id='phone'
-              defaultValue={inputPhone}
-              autoComplete='on'
-              onChange={(e) => setInputPhone(e.target.value)}
-              required
-            />
-            <FormLabel htmlFor='city'> City </FormLabel>
-            <FormInput
-              id='city'
-              defaultValue={inputCity}
-              autoComplete='on'
-              onChange={(e) => setInputCity(e.target.value)}
-              required
-            />
-            <FormLabel htmlFor='city'> Country </FormLabel>
-            <FormInput
-              id='country'
-              defaultValue={inputCountry}
-              autoComplete='on'
-              onChange={(e) => setInputCity(e.target.value)}
-              required
-            />
-            <FormButton type='submit' onClick={handleFetchData}>
-              Save
-            </FormButton>
-                </Form>
-            </FormContent>
-        </FormWrap>
-        <FormWrap>
-            <FormContent>
-                <Form action="#">
-                    <FormH1> Plants I Added </FormH1>
-                    <ul>
-                      {statePlants.map((val) => (
-                        <li key={val._id}>{val.name}</li>
-                      ))}
-                      </ul>
-                      </Form>
-            </FormContent>
-        </FormWrap>
-    </Container>
-  )
-}
->>>>>>> 2c09b9933050870816aba3b1dd88fe68b79fb05e
 
         try {
             const axiosInstance = createAxiosInstance();
